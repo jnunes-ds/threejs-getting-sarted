e0f7300e0620dc8bade1fb642c7a0ef2638dd606
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-29 01:17:55 -0300

    unnecessary files removed

--- src/BlasterScene.ts
+++ src/BlasterScene.ts
@@ -1,26 +1,52 @@
 import * as Three from "three";
 
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 class BlasterScene extends Three.Scene {
   private readonly mtlLoader = new MTLLoader();
   private readonly objLoader = new OBJLoader();
 
-  initialize() {
-    const geometry = new Three.BoxGeometry();
-    const material = new Three.MeshPhongMaterial({ color: 0xffad00 });
+  async initialize() {
+    const targetMtl = await this.mtlLoader.loadAsync(
+      "assets/kenney_blaster-kit/OBJ_format/targetA.mtl"
+    );
+    targetMtl.preload();
 
-    const cube = new Three.Mesh(geometry, material);
-    cube.position.z = -5;
-    cube.position.y = -1;
+    const target1 = await this.createTarget(targetMtl);
+    target1.position.x = -1;
+    target1.position.z = -3;
 
-    this.add(cube);
+    const target2 = await this.createTarget(targetMtl);
+    target2.position.x = 1;
+    target2.position.z = -3;
+
+    const target3 = await this.createTarget(targetMtl);
+    target3.position.x = 2;
+    target3.position.z = -3;
+
+    const target4 = await this.createTarget(targetMtl);
+    target4.position.x = 21;
+    target4.position.z = -3;
+
+    this.add(target1, target2, target3, target4);
 
     const light = new Three.DirectionalLight(0xffffff, 1);
     light.position.set(0, 4, 2);
 
     this.add(light);
+  }
+
+  private async createTarget(mtl: MTLLoader.MaterialCreator) {
+    this.objLoader.setMaterials(mtl);
+
+    const modelRoot = await this.objLoader.loadAsync(
+      "assets/kenney_blaster-kit/OBJ_format/targetA.obj"
+    );
+
+    modelRoot.rotateY(Math.PI * 0.5);
+
+    return modelRoot;
   }
 
   update() {

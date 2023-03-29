5fd3bf3ad3e9f9fe46ee3dfb02315e6c1a0babfb
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-29 00:42:08 -0300

    box  and light added to scene

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,18 @@ const mainCamera = new Three.PerspectiveCamera(60, width / height, 0.1, 100);
 
 const scene = new Three.Scene();
 
+const geometry = new Three.BoxGeometry();
+const material = new Three.MeshPhongMaterial({ color: 0xffad00 });
+
+const cube = new Three.Mesh(geometry, material);
+cube.position.z = -5;
+cube.position.y = 1;
+
+scene.add(cube);
+
+const light = new Three.DirectionalLight(0xffffff, 1);
+light.position.set(0, 4, 2);
+
+scene.add(light);
+
 renderer.render(scene, mainCamera);

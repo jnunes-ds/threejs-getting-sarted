bf967aaa0e7a96384c839c6c93862c579c9d6a6d
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-29 00:32:15 -0300

    setting canvas size

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import * as Three from "three";
+
+const { innerWidth: width, innerHeight: height } = window;
+
+const render = new Three.WebGL1Renderer({
+  canvas: document.getElementById("app") as HTMLCanvasElement,
+});
+
+render.setSize(width, height);
